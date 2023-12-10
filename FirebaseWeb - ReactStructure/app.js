910bb6e0c27a1df263c107f997cf6cd041366e87
telezs <86789332+telezs@users.noreply.github.com>
2023-12-10 18:06:22 +0700

    Add files via upload

---
+++ FirebaseWeb - ReactStructure/app.js
@@ -0,0 +1,57 @@
+class App {
+  activeScreen;
+  container;
+  nav;
+  main;
+
+  constructor(container) {
+    this.container = container;
+  }
+
+  changeActiveScreen(screen) {
+    // todo
+    if(this.activeScreen) is = undefined {
+      this.container.innerHTML = "";
+    }
+    this.activeScreen = screen;
+    this.activeScreen.initRender(this.container);
+  }
+}
+const container = document.getElementById("app");
+const app = new App(container);
+
+// check file to load html code
+if (location.href.substring(21) == "/index.html") {
+  // todo
+  const login = new Login();
+  app.changeActiveScreen(login);
+}
+if (location.href.substring(21) == "/pages/home.html") {
+  // todo
+}
+
+//export instant của app chứ ko export class vì App là duy nhất
+export default app;
+
+// Init firebase app
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore-compat.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOd2bK7XwuJpXE-TDRkX-qACdZKZS551U",
+  authDomain: "jsi03-blogweb.firebaseapp.com",
+  databaseURL: "https://jsi03-blogweb-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "jsi03-blogweb",
+  storageBucket: "jsi03-blogweb.appspot.com",
+  messagingSenderId: "818746374260",
+  appId: "1:818746374260:web:8ae613af33e5dc057ef921"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const firestore = getFirestore(firebaseApp);
---
+++ FirebaseWeb - ReactStructure/components/nav/nav.js
@@ -0,0 +1,14 @@
+function getUsernameLetters() {
+  // todo
+  return "AB";
+}
+
+document.getElementById("account-icon").innerHTML = getUsernameLetters();
+
+function logout(e) {
+  // todo
+}
+
+document.getElementById("logout-btn").addEventListener("click", (e) => {
+  logout(e);
+});
